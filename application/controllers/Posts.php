340b3b5278290c5a9d23ad1ccfebdbae7170ed77
Erine Natnat <enatnat.08@gmail.com>
2020-05-29 20:20:04 -0700

    Routing and views

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -12,4 +12,20 @@
             $this->load->view('posts/index', $data);
             $this->load->view('templates/footer');
         }
+
+        //Linking views
+        public function view($slug = NULL){
+            $data['post'] = $this->post_model->get_posts($slug); 
+
+            if(empty($data['post'])) { #if no slug, show 404 error message
+                show_404();
+            }
+            #Else set post title to the data title
+            $data['title'] = $data['post']['title'];
+
+            #Load our views
+            $this->load->view('templates/header');
+            $this->load->view('posts/view', $data);
+            $this->load->view('templates/footer');
+        }
     }
