e26f0375369beac31482cc16fb0f04a17a8a4835
Erine Natnat <enatnat.08@gmail.com>
2020-05-29 19:56:53 -0700

    Add blog link

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,12 +10,13 @@
     <nav class="navbar navbar-inverse">
         <div class="container">
             <div class="navbar-header">
-                <a href="#" class="navbar-brand">ciBlog</a>
+                <a href="<?php echo base_url(); ?>" class="navbar-brand">ciBlog</a>
             </div>
             <div id="navbar">
                 <ul class="nav navbar-nav">
-                    <li><a href="<?php echo base_url();?>">Home</a></li>
-                    <li><a href="<?php echo base_url();?>about">About</a></li>
+                    <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                    <li><a href="<?php echo base_url(); ?>about">About</a></li>
+                    <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
                 </ul>
             </div>
         </div>
