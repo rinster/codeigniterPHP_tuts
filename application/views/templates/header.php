538167a72b7ec8370739d66e94ac454083e550c7
Erine Natnat <enatnat.08@gmail.com>
2020-06-03 13:36:27 -0700

    Posts - link to create posts view

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -18,6 +18,9 @@
                     <li><a href="<?php echo base_url(); ?>about">About</a></li>
                     <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
                 </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="<?php echo base_url(); ?>posts/create">Create Post</a></li>
+                </ul>
             </div>
         </div>
     </nav>  
