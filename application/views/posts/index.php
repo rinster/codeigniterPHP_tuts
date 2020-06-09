0b30d9d35440feb43b9b8d78b0c424b4602bf8ee
Erine Natnat <enatnat.08@gmail.com>
2020-06-09 00:41:43 -0700

    Posts - helper to limit post description words

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,7 +3,7 @@
     <h3><?php echo $post['title']; ?></h3>
     <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small>
     <br>
-    <?php echo $post['body']; ?>
+    <?php echo word_limiter($post['body'], 60); ?>
     <br><br>
     <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read more</a></p>
 <?php endforeach; ?>
